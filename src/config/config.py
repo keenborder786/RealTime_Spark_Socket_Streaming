a6c60aebf2734679341a71722e47e66e3180b737
keenborder786 <21110290@lums.edu.pk>
2024-05-30 15:15:38 +0500

    [chore]

---
+++ src/config/config.py
@@ -0,0 +1,18 @@
+config = {
+    "openai": {
+        "api_key": "OPENAI_KEY"
+    },
+    "kafka": {
+        "sasl.username": "X2JHUQGEUAQH764Y",
+        "sasl.password": "uPOVsOUTGt58C/nuX04u2EHrWSXb+RmaaxnA9hWrlRL8LHEC8AubsAGqf114kv/R",
+        "bootstrap.servers": "pkc-4j8dq.southeastasia.azure.confluent.cloud:9092",
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'session.timeout.ms': 50000
+    },
+    "schema_registry": {
+        "url": "https://psrc-nvgyv3.southeastasia.azure.confluent.cloud",
+        "basic.auth.user.info": "CB3QWBHEMYUOZO5B:GCm5SWQT0h+6atnhJR+Gan+SjP90tmLZ0eLR/GA0FR2F4fZYcv7zKu/GeB4UAhGy"
+
+    }
+}
